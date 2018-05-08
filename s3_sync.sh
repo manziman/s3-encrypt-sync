85c6d0af8214559c3e0609dca057e1078f3293af
themanzi <christophermmanzi@protonmail.com>
2018-05-08 13:32:29 -0500

    added aws credentials

--- s3_sync.sh
+++ s3_sync.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Construct necessary configs
 if [ ! -f ${SCRIPT_DIR}/s3_sync.config  ]; then
     while true; do
         read -p "Config not found, set up now?(y/n)" yn
@@ -13,7 +14,7 @@ if [ ! -f ${SCRIPT_DIR}/s3_sync.config  ]; then
     done
     while true; do
         read -p "Please input username: " usr
-        id -u ${usr}
+        id -u ${usr} >> /dev/null
         if [ ! $? == 0 ]; then
             echo "User ${usr} not found! Please enter valid user."; continue;
         else
@@ -28,6 +29,40 @@ if [ ! -f ${SCRIPT_DIR}/s3_sync.config  ]; then
             break
         fi
     done
+
+    while true; do
+        read -p "Configure AWS profile/credentials now?(Y/n)" yn
+        case $yn in
+            [Nn]* ) read -p "Please enter existing AWS profile name: " profile;;
+            [Yy]* ) read -p "Please enter the name for the new AWS profile: " newprofile;;
+            * ) echo "Please input yes or no."; continue;;
+        esac
+        if [ $profile  ]; then
+            grep "\[profile ${profile}\]" /home/${usr}/.aws/config
+            if ! [ $? == 0 ]; then
+                echo "Profile ${profile} not found!"; continue;
+            fi
+        else
+            aws configure --profile $newprofile
+            if ! [ $? == 0  ]; then
+                continue
+            else
+                profile=$newprofile
+                break
+            fi
+        fi
+    done
+
+    # S3 bucket name
+    while true; do
+        read -p "Please enter name of s3 bucket to use: " s3_bucket
+        if ! [ s3_bucket ]; then
+            echo "Please enter a bucket name."; continue;
+        else
+            break
+        fi
+    done
+
     DIRS=""
     while true; do
         read -p "Enter a directory to backup (<Enter> when done): " newdir
@@ -48,14 +83,21 @@ if [ ! -f ${SCRIPT_DIR}/s3_sync.config  ]; then
     echo "GPG_KEY_ID=\"${email}\"" >> ${SCRIPT_DIR}/s3_sync.config
     echo "# [directories]" >> ${SCRIPT_DIR}/s3_sync.config
     echo "DIRECTORIES=\"${DIRS[@]}\"" >> ${SCRIPT_DIR}/s3_sync.config
+    echo "# [aws]" >> ${SCRIPT_DIR}/s3_sync.config
+    echo "AWS_PROFILE=\"${profile}\"" >> ${SCRIPT_DIR}/s3_sync.config
+    echo "S3_BUCKET=\"${s3_bucket}\"" >> ${SCRIPT_DIR}/s3_sync.config
 fi
 
+# Read the config file and source it
 echo "Reading config file ${SCRIPT_DIR}/s3_sync.config..."
 source ${SCRIPT_DIR}/s3_sync.config
 
-cat ${SCRIPT_DIR}/s3_sync.config
+# Validate AWS credential permissions
 
+# Create cronjob if needed
+
+# Iterate through directories and sync
 IFS=","
-for i in $DIRECTORIES; do
-    echo $i
+for d in $DIRECTORIES; do
+    echo $d
 done
