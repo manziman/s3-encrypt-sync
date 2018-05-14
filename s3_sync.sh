2d6bccfbec49ff17500a26b076a39daa58ebc568
themanzi <christophermmanzi@protonmail.com>
2018-05-14 16:36:59 -0500

    changed directories

--- s3_sync.sh
+++ s3_sync.sh
@@ -13,12 +13,7 @@ source ${SCRIPT_DIR}/s3_sync.config
 IFS=","
 for dir in $DIRECTORIES; do
     remote_dir=$(echo $dir | sed 's/.*\///g')
-    mkdir /tmp/$remote_dir
-    find $dir -type f -not -name "*.gpg" -exec cp --parents {} /tmp/$remote_dir/ \;
-    find /tmp/$remote_dir/ -type f -not -name "*.gpg" -exec gpg --batch --yes --output {}.gpg --recipient christophermmanzi@protonmail.com --encrypt {} \;
-    find /tmp/$remote_dir -type f -not -name "*.gpg" -exec shred -zvu {} > /dev/null \;
-    aws s3 sync --include "*.gpg" /tmp/$remote_dir s3://$S3_BUCKET/$remote_dir
-    find /tmp/$remote_dir/ -type f -name "*.gpg" -exec cp {} $dir \;
-    rm -rf /tmp/$remote_dir
+    find $dir -type f -not -name "*.gpg" -exec gpg --batch --yes --output {}.gpg --recipient christophermmanzi@protonmail.com --encrypt {} \;
+    aws s3 sync --include "*.gpg" $dir s3://$S3_BUCKET/$remote_dir
 done
 
